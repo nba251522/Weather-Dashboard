5db2f6783bf6ccc3e3b1e7ecf60d1462624a400b
nba251522 <inquister112345678@gmail.com>
2023-10-10 18:37:02 -0700

    added a function that fetches the weather data from the api. I also added an error code that will pop up if there are issues on obtaining the information from the api

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,21 @@
 const searchButton = document.getElementById('search-button');
 const cityInput = document.getElementById('city-input');
 const API_KEY = 'd17a9d0a1cf31171feebed2714a02008';     
+
+searchButton.addEventListener('click', fetchWeatherData);
+function fetchWeatherData() {
+    const cityName = cityInput.value;
+    const endpoint = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&appid=${API_KEY}`;
+
+    fetch(endpoint)
+        .then(response => response.json())
+        .then(data => {
+            displayCurrentWeather(data);
+            displayForecast(data);
+            storeSearchHistory(cityName);
+        })
+        .catch(error => {
+            console.error('Error fetching weather data:', error);
+            alert('Error fetching weather data. Please try again.');
+        });
+}
