977394e8e9acca05092d9358e85742307bb9b4fa
nba251522 <inquister112345678@gmail.com>
2023-10-10 19:00:49 -0700

    added functions that display the forecast, search history, and store search history. I also linked the js to the html so now all elements are working as intended

--- Develop/script.js
+++ Develop/script.js
@@ -16,6 +16,52 @@ function fetchWeatherData() {
         })
         .catch(error => {
             console.error('Error fetching weather data:', error);
-            alert('Error fetching weather data. Please try again.');
+            alert('Error fetching weather data. Please try again later.');
         });
 }
+function displayCurrentWeather(data) {
+    document.getElementById('city-name-date').textContent = `${data.city.name} (${new Date().toLocaleDateString()})`;
+    document.getElementById('weather-icon').src = `https://openweathermap.org/img/w/${data.list[0].weather[0].icon}.png`;
+    document.getElementById('temperature').textContent = `Temperature: ${data.list[0].main.temp}°F`;
+    document.getElementById('humidity').textContent = `Humidity: ${data.list[0].main.humidity}%`;
+    document.getElementById('wind-speed').textContent = `Wind Speed: ${data.list[0].wind.speed} m/s`;
+}
+
+function displayForecast(data) {
+    for (let i = 0; i < 5; i++) {
+        const forecastData = data.list[(i + 1) * 8];
+        const dayElem = document.getElementById(`day${i + 1}`);
+
+        dayElem.querySelector('.forecast-date').textContent = new Date(forecastData.dt * 1000).toLocaleDateString();
+        dayElem.querySelector('.forecast-icon').src = `https://openweathermap.org/img/w/${forecastData.weather[0].icon}.png`;
+        dayElem.querySelector('.forecast-temperature').textContent = `Temperature: ${forecastData.main.temp}°F`;
+        dayElem.querySelector('.forecast-wind-speed').textContent = `Wind Speed: ${forecastData.wind.speed} m/s`;
+        dayElem.querySelector('.forecast-humidity').textContent = `Humidity: ${forecastData.main.humidity}%`;
+    }
+}
+
+function storeSearchHistory(cityName) {
+    let searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+    if (searchHistory.indexOf(cityName) === -1) {
+        searchHistory.push(cityName);
+        localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+        displaySearchHistory();
+    }
+}
+
+function displaySearchHistory() {
+    const searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+    const historyList = document.getElementById('search-history-list');
+    historyList.innerHTML = '';
+    for (const city of searchHistory) {
+        const li = document.createElement('li');
+        li.textContent = city;
+        li.addEventListener('click', () => {
+            cityInput.value = city;
+            fetchWeatherData();
+        });
+        historyList.appendChild(li);
+    }
+}
+
+displaySearchHistory();
